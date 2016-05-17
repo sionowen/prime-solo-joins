55caac72edeb2138c64737be6dbcf31fde960321
sionowen <sionowen34@gmail.com>
2016-05-17 17:39:47 -0500

    improved an answer

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,23 +1,28 @@
 --Get all customers and their addresses.
-SELECT * FROM customers
+SELECT first_name, last_name, street, city, state, zip FROM customers
 	join addresses ON customers.id = addresses.id
 --Get all orders and their line items.
 SELECT * FROM orders
 	join line_items ON orders.id = line_items.id
 --Which warehouses have cheetos?
-SELECT * FROM warehouse_product
+SELECT warehouse, description FROM warehouse_product
 	JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id
 	JOIN products ON warehouse_product.product_id = products.id
 	WHERE description = 'cheetos'
 --Which warehouses have diet pepsi?
-SELECT * FROM warehouse_product
+SELECT warehouse, description FROM warehouse_product
 	JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id
 	JOIN products ON warehouse_product.product_id = products.id
 	WHERE description = 'diet pepsi'
 --Get the number of orders for each customer. NOTE: It is OK if those without orders are not --included in results.
 SELECT * FROM addresses
-	JOIN customers ON addresses.customer_id = customers.id
-	JOIN orders ON addresses.id = orders.address_id
+SELECT customers.first_name, COUNT(first_name)
+FROM customers
+JOIN addresses
+ON addresses.customer_id = customers.id
+JOIN orders
+ON orders.address_id = addresses.id
+GROUP BY first_name
 --How many customers do we have?
 SELECT COUNT(*) FROM customers
 --How many products do we carry?
